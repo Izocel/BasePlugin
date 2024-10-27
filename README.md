6b51da83bbf16ba277559f5a238a65f0b7376f9a
Raphael Vigneault Ð. <triangle1232009@hotmail.com>
2024-10-27 12:29:28 -0400

    ....

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 - edit `createDocker.sh` (user, pass, VOLPATH)
-- run it e.g: `./createDocker.sh "TestServer" "CS2-TestServer" 27015 27020`
+- run it e.g: `./createDocker.sh
 - code your plugin
 - vscode-task: `publish` (will release your plugin into the created docker)
--- createDocker.sh
+++ createDocker.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# ./createDocker.sh "TestServer" "CS2-TestServer" 27015 27020
+# webdevteam D5A7D232A215A50940921C542C14290A
+read -p "Steam user (webdevteam):" STEAMUSER
+read -p "Steam password:" STEAMPASS
+read -p "Steam token:" TOKEN
+read -p "Steam port (27015):" GPORT
+read -p "Steam tv_port (27016):" TPORT
+read -p "Container name (TestServer):" CONTNAME
+read -p "Server name (TestServer):" SERVERNAME
+read -p "Username folderpath (izocel):" USERFOLDER
 
-if [[ -z "$1" || -z "$2" || -z "$3" || -z "$4" ]]; then
-    echo "Invalid arguments supplied"
-fi
-
-APIKEY=""
-TOKEN=$TTOK
-STEAMUSER="username"
-STEAMPASS="userpass"
-STEAMGUARD="usercodemail"
-VOLPATH="C:\projects\CS2-Plugins\BasePlugin\server"
-
-CONTNAME=$1
-SERVERNAME=$2
-GPORT=$3
-TPORT=$4
+CONTNAME="CS2-$CONTNAME"
+HOMESPATH="/volume1/homes"
+VOLPATH="$HOMESPATH/$USERFOLDER/docker/$CONTNAME"
+RCONPASS=$(head -c 16 /dev/random | md5sum | cut -f 1 -d\ )
+SRVRPASS=$(head -c 16 /dev/random | md5sum | cut -f 1 -d\ )
 
 REGION=0
 MAXPLAYER=12
@@ -29,21 +27,20 @@ WCOL=0
 WMAP=0
 ARGS=""
 
-RNDPASS=$(head -c 16 /dev/random | md5sum | cut -f 1 -d\ )
-RNDPASS2=$(head -c 16 /dev/random | md5sum | cut -f 1 -d\ )
-CONTNAME="CS2-$CONTNAME"
-
 echo ""
-echo "Container name (lc): $CONTNAME"
+echo "Container name: $CONTNAME"
 echo "Volume path: $VOLPATH"
 echo "Server name: $SERVERNAME"
 echo "Game port: $GPORT"
 echo "Tv-Game port: $TPORT"
-echo "Game Token: $TTOK"
+echo "Game Token: $TOKEN"
+echo "RCON-PASS: $RCONPASS"
+echo "SERVER-PASS: $SRVRPASS"
 echo ""
 
-read -p "Confirm configs (y/n)?" CONT
-if [ "$CONT" != "y" ]; then
+
+read -p "Confirm configs (y/n)?" CONTINUE
+if [ "$CONTINUE" != "y" ]; then
     echo "Exiting..."
     exit
 fi
@@ -53,14 +50,20 @@ mkdir -p "$VOLPATH/steam" && chmod 777 "$VOLPATH/steam"
 docker volume create --name $CONTNAME-USER --opt type=none --opt device="$VOLPATH/user" --opt o=bind
 docker volume create --name $CONTNAME-STEAM --opt type=none --opt device="$VOLPATH/steam" --opt o=bind
 
+read -p "Confirm volumes OK (y/n)?" CONTINUE
+if [ "$CONTINUE" != "y" ]; then
+    echo "Exiting..."
+    exit
+fi
+
 echo ""
 docker run -it -d --name="SteamCMD" -v "SteamCMD:/home/steam/Steam" cm2network/steamcmd
 docker exec -it "SteamCMD" sh -c "chmod 755 /home/steam/steamcmd/steamcmd.sh && /home/steam/steamcmd/steamcmd.sh +login $STEAMUSER $STEAMPASS +quit"
 docker stop SteamCMD && docker rm SteamCMD
 
 echo ""
-read -p "Steam cmd login OK (y/n)?" CONT
-if [ "$CONT" != "y" ]; then
+read -p "Confirm steam cmd login OK (y/n)?" CONTINUE
+if [ "$CONTINUE" != "y" ]; then
     echo "Exiting..."
     exit
 fi
@@ -73,8 +76,8 @@ docker run -it -d --net=bridge -p $GPORT:$GPORT/tcp -p $GPORT:$GPORT/udp -p $TPO
     -v "$CONTNAME-STEAM:/home/steam/" \
     -v "SteamCMD:/home/steam/Steam" \
     -e STEAMUSER=$STEAMUSER \
-    -e CS2_RCONPW=$RNDPASS \
-    -e CS2_PW=$RNDPASS2 \
+    -e CS2_RCONPW=$RCONPASS \
+    -e CS2_PW=$SRVRPASS \
     -e CS2_LAN="0" \
     -e CS2_FPSMAX=128 \
     -e CS2_TICKRATE=512 \
@@ -94,8 +97,7 @@ docker exec -it --user=0 "$CONTNAME" sh -c "dpkg --configure -a && apt-get updat
 
 unset STEAMUSER
 unset STEAMPASS
-unset RNDPASS
-unset RNDPASS2
+unset RCONPASS
+unset SRVRPASS
 unset VOLPATH
 unset TOKEN
-unset APIKEY
